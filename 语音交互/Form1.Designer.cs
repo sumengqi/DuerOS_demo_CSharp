f8beb774f754dd1d77e9ebd9d22d23ed41ede530
sumengqi <sumq@qq.com>
2017-07-31 17:20:20 +0800

    dueros deo

---
+++ 语音交互/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace 语音交互
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(105, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "按住我和我对话";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 386);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(385, 96);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 517);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "By 香酥肉饼";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 10);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(385, 370);
+            this.webBrowser1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DuerOS测试";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ 语音交互/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management; 
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+using System.Media;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NAudio.Wave;
+using NAudio.Codecs;
+using NAudio.FileFormats;
+using NAudio.CoreAudioApi;
+using NAudio.Dmo;
+using NAudio.Dsp;
+using NAudio.Gui;
+using NAudio.Midi;
+using NAudio.Mixer;
+using NAudio.Sfz;
+using NAudio.Utils;
+using System.Net.NetworkInformation;
+using System.Collections;
+
+namespace 语音交互
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IWaveIn waveIn;
+        private WaveFileWriter writer;
+        /// <summary>
+        /// 开始录音
+        /// </summary>
+        private void StartRecording()
+        {
+            if (waveIn != null) return;
+            waveIn = new WaveIn { WaveFormat = new WaveFormat(16000, 1) };//设置码率
+            writer = new WaveFileWriter(System.IO.Path.GetTempPath()+"\\rec.wav", waveIn.WaveFormat);
+            //writer = new WaveFileWriter("1.wav", waveIn.WaveFormat);
+            waveIn.DataAvailable += waveIn_DataAvailable;
+            waveIn.RecordingStopped += OnRecordingStopped;
+            waveIn.StartRecording();
+        }
+        /// <summary>
+        /// 停止录音
+        /// </summary>
+        private void StopRecording()
+        {
+            waveIn.StopRecording();
+            waveIn.Dispose();
+            writer.Close();
+        }
+        /// <summary>
+        /// 录音中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                int secondsRecorded = (int)(writer.Length / writer.WaveFormat.AverageBytesPerSecond);//录音时间获取
+                if (secondsRecorded >= 30)
+                {
+                    StopRecording();
+                }
+
+            }
+        }
+        /// <summary>
+        /// 停止录音
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<StoppedEventArgs>(OnRecordingStopped), sender, e);
+            }
+            else
+            {
+                if (waveIn != null) // 关闭录音对象
+                {
+                    waveIn.Dispose();
+                    waveIn = null;
+                }
+                if (writer != null)//关闭文件流
+                {
+                    writer.Close();
+                    writer = null;
+                }
+                if (e.Exception != null)
+                {
+                    MessageBox.Show(String.Format("出现问题 {0}",
+                                                  e.Exception.Message));
+                }
+            }
+        }
+            
+        public  byte[] HttpPost(string url, byte[] postData)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("authorization", "Bearer 24.686b1ad5963dbf8157444a9b19ab1324.2592000.1503723917.282335-9845303");//token，替换成自己的
+            webClient.Headers.Add("dueros-device-id", "879622951");//device-id
+            webClient.Headers.Add("content-type", "multipart/form-data; boundary=--879622951");//boundary
+            byte[] responseData = webClient.UploadData(url, "POST", postData);//得到返回字符流  
+            webClient.Dispose();
+            return responseData;
+        }
+ 
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            StartRecording();
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            axWindowsMediaPlayer1.currentPlaylist.clear();
+            StopRecording();
+            string url = "http://dueros-h2.baidu.com/dcs/v1/events";
+            string path = "postbody.dat";  //postbody
+            FileStream fs = new FileStream(path, FileMode.Open);
+            //获取文件大小
+            long size = fs.Length;
+            byte[] postData = new byte[size];
+            //将文件读到byte数组中
+            fs.Read(postData, 0, postData.Length);
+            fs.Close();
+            byte[] end = Encoding.Default.GetBytes("\r\n----879622951--");
+            string path1 = System.IO.Path.GetTempPath() + "\\rec.wav";
+            FileStream fs1 = new FileStream(path1, FileMode.Open);
+            //获取文件大小
+            long size1 = fs1.Length;
+            byte[] sound = new byte[size1];
+            //将文件读到byte数组中
+            fs1.Read(sound, 0, sound.Length);
+            fs1.Close();
+            byte[] resArr = new byte[postData.Length + sound.Length + end.Length];
+            postData.CopyTo(resArr, 0);
+            sound.CopyTo(resArr, postData.Length);
+            end.CopyTo(resArr, postData.Length + sound.Length);
+            byte[] http_byte = HttpPost(url, resArr);
+            string http = Encoding.Default.GetString(http_byte);
+            string[] body = http.Split(new string[1] { "--___dumi_avs_xuejuntao___\r\n" }, StringSplitOptions.None);
+            ArrayList al = new ArrayList();
+            ArrayList al_j = new ArrayList();
+            for (int i = 1; i < body.Length - 1; i++)
+            {
+                al.Add(body[i].Split(new string[1] { "\r\n\r\n" }, StringSplitOptions.None));
+            }
+            string[][] http_body = new string[al.Count][];
+            al.CopyTo(http_body);
+            for (int i = 0; i < http_body.Length; i++)
+            {
+                if (http_body[i][0].IndexOf("application/json") >= 0)
+                {
+                    al_j.Add((JObject)JsonConvert.DeserializeObject(http_body[i][1]));
+                }
+                else if (http_body[i][0].IndexOf("application/octet-stream") >= 0)
+                {
+                    FileStream fi = new FileStream(System.IO.Path.GetTempPath() + "\\Speak.mp3", FileMode.Create);
+                    fi.Write(http_byte, 0, http_byte.Length);
+                    fi.Close();
+                }
+            }
+            JObject[] json_body = new JObject[al_j.Count];
+            al_j.CopyTo(json_body);
+ 
+            axWindowsMediaPlayer1.currentPlaylist = axWindowsMediaPlayer1.newPlaylist("sound", "");
+            for (int i = 0; i < json_body.Length; i++)
+            {
+                switch (json_body[i]["directive"]["header"]["name"].ToString())
+                {
+                    case "Speak":
+                        //file_put_contents("Speak.mp3",$voice);
+                        string speak_url = System.IO.Path.GetTempPath() + "\\Speak.mp3";
+                        axWindowsMediaPlayer1.currentPlaylist.appendItem(axWindowsMediaPlayer1.newMedia(speak_url));
+                        break;
+                    case "Play":
+                        string play_url = json_body[i]["directive"]["payload"]["audioItem"]["stream"]["url"].ToString();
+                        axWindowsMediaPlayer1.currentPlaylist.appendItem(axWindowsMediaPlayer1.newMedia(play_url));
+                        break;
+                    case "HtmlView":
+                        string view_url = json_body[i]["directive"]["payload"]["url"].ToString();
+                        webBrowser1.ScriptErrorsSuppressed = true;
+                        webBrowser1.Navigate(view_url);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+    }
+}
